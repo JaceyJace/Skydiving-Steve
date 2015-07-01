2516280696e2f53e5b80ce9adc571c902a5db296
CalebSurman <crimsanity76@gmail.com>
2015-07-01 19:45:01 +1000

    fixed gamestates bug properly

--- main.js
+++ main.js
@@ -79,6 +79,10 @@ var FRICTION = MAXDX * 30;//6;
 
 //var player.isDead == true
 
+var STATE_GAME = 0;
+var STATE_GAMEOVER = 1;
+var STATE_GAMEWIN = 2;
+
 //SIDESCROLLING AND COLLISION
 function cellAtPixelCoord(layer, x, y)
 {
